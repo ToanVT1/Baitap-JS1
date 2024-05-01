6a614f763bef96053eb710de141f4caf9f4a6e6b
toan <thanhtoan12x3clc@gmail.com>
2024-05-02 00:44:09 +0700

    commit bai 1 JS lan 1

---
+++ JV/index.js
@@ -0,0 +1,13 @@
+console.log("Bai3: Quy doi tien");
+var TiGia = 23500;
+var SoTienNhap = 2;
+var TienViet = TiGia * SoTienNhap;
+console.log("Dap an:", TienViet);
+
+console.log("Bai4: Tinh dien tich chu vi");
+var Dai = 100;
+var Rong = 50;
+var Dientich = Dai * Rong;
+var ChuVi = (Dai + Rong)*2;
+console.log("Dien tich hcn:", Dientich);
+console.log ("Chu vi HCN:", ChuVi);
